572935d8c09e90bb1c89645acc2d443fc4947154
Amber Prince <amber.prince75@yahoo.com>
2021-04-06 12:31:26 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -46,22 +46,21 @@ function displayEmployees(employeeData) {
 function displayModal(index) {
 
     //use object destructuring make our template literal cleaner
-    let {name, dob, phone, email, location: { city, street, state, postcode
-    }, picture} = employees[index];
+    let {name, dob, phone, email, location: { city, street, state, postcode }, picture} = employees[index];
 
-    let date = newDate(dob.date);
+    let date = new Date(dob.date);
 
     const modalHTML = `
         <img class="avatar" src="${picture.large}">
         <div class="text-container">
-                <h2 class="name">${name.first} ${name.last}</h2>
-                <p class="email">${email}</p>
-                <p class="address">${city}</p>
-                <hr />
-                <p>${phone}</p>
-                <p class="address">${street}, ${state}, ${postcode},</p>
-                <p>Birthday: 
-                ${date.getMonth()}/${date.getDate()}/${date.getFullYear}</p>
+            <h2 class="name">${name.first} ${name.last}</h2>
+            <p class="email">${email}</p>
+            <p class="address">${city}</p>
+            <hr />
+            <p>${phone}</p>
+            <p class="address">${street}, ${state}, ${postcode},</p>
+            <p>Birthday: 
+            ${date.getMonth()}/${date.getDate()}/${date.getFullYear}</p>
         </div>
 
     `;
